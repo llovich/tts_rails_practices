389d6faae89c876e8dc82c2a9c3241916f4daf20
girllogic <lillianlovich@yahoo.com>
2016-02-15 08:52:39 -0600

    added links and new gems

--- doggie_daycare/config/routes.rb
+++ doggie_daycare/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dog_care/index'
 
-  get 'dog_care/about'
+  get '/about' => 'dog_care#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
