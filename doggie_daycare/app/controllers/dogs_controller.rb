9c930d3cf6b3d5e2bd3b17271df4544114584024
girllogic <lillianlovich@yahoo.com>
2016-02-16 09:55:25 -0600

    more stuff

--- doggie_daycare/app/controllers/dogs_controller.rb
+++ doggie_daycare/app/controllers/dogs_controller.rb
@@ -1,10 +1,39 @@
 class DogsController < ApplicationController
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
   def index
     @dogs = Dog.all
   end
 
   def new
     @dog = Dog.new
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    if @dog.update(dog_params)
+      redirect_to dog_url(@dog), notice: "Dog updated!"
+    else
+      flash.now[:notice] = "Could not update dog :("
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+
+    if @dog.destroy
+      redirect_to dogs_url, notice: "Successfully deleted!"
+    else
+      redirect_to dogs_url, notice: "Could not delete dog."
+    end
   end
 
   def create
@@ -14,11 +43,12 @@ class DogsController < ApplicationController
       else
       flash.now[:notice] = "Could not save dog. :("
       render :new
-      end
     end
+  end
 
-    private
-
+  private
+    #important - white listed params - set only the ones you want to save
+    #also security/blocks users from injecting nefarious data into db
     def dog_params
     params.require(:dog).permit(:name, :breed, :age)
   end
--- doggie_daycare/config/routes.rb
+++ doggie_daycare/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs, only:[:index, :new, :create]
+  resources :dogs
 
   get 'dog_care/index'
 
