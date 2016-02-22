7f250210a8a02e1ef31190862f926476d447ea19
girllogic <lillianlovich@yahoo.com>
2016-02-22 09:57:11 -0600

    added scope, fixed bugs

--- music_app/app/models/genre.rb
+++ music_app/app/models/genre.rb
@@ -2,4 +2,11 @@ class Genre < ActiveRecord::Base
   has_many :artists
   has_many :songs, through: :artists
   validates :name, presence: true, length: {minimum: 3}, uniqueness: true
+  #gives items created within the last indicate minutes
+  # -> / the lambda means it will refresh every time it is run
+
+  scope :recent, -> (minutes_past=60) {where("created_at > ?", minutes_past.minutes.ago)}
+  scope :today, -> {where('DATE(created_at)=?', Date.today)}
+
+  scope :last_five, -> {order(:created_at => :desc).limit(5)}
 end
